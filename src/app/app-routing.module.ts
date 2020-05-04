36ecafb34fe3cb0f874796222ed1109ac03b91e9
Nicolas Garcia <nicolasgarciafranco@gmail.com>
2020-05-03 23:59:40 -0500

    primer proyecto ANGULAR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
+import { IndexComponent } from './index/index.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ContactoComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
